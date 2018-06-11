1884f68a01193a84cfacb5f338fa843624d15a6b
russomp <russomp@usc.edu>
2018-06-11 19:25:45 -0400

    finish ex9: Printing, printing, printing

---
+++ ex9.rb
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fir Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nnAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+There's something going on here.
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
