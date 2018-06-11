43db7733b44cff3453f0365b62cd33a37338cf45
russomp <russomp@usc.edu>
2018-06-11 10:29:23 -0400

    finish ex1: A good first program

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts 'Another line for fun.'
+#puts 'An "octothorpe" is also called a "pound", "hash", "mesh", or any number of names. Pick the one that makes you chill out.'
