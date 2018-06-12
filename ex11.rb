563b631b884ea3accbe68df639e12c4e08474e67
russomp <russomp@usc.edu>
2018-06-12 10:49:00 -0400

    finish ex11: Asking questions

---
+++ ex11.rb
@@ -0,0 +1,13 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+print "What is your hair color? "
+hair_color = gets.chomp
+print "What is your eye color? "
+eye_color = gets.chomp
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+puts "So, you also have #{hair_color} hair and #{eye_color} eyes."
